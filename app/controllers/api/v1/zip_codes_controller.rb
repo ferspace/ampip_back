d2395c7a879d336a63c1d58c37fc27627678593e
fer <>
2021-08-15 14:35:15 -0500

    conflicts

--- app/controllers/api/v1/zip_codes_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Api::V1::ZipCodesController < ApplicationController
-  # GET '/zip_codes'
-  def index
-    render json: ZipCode.search(params)
-  end
-end
--- app/serializers/api/v1/city_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Api::V1::CitySerializer < Api::V1::ActiveModel::Serializer
-  attributes :id, :name, :state_id
-end
