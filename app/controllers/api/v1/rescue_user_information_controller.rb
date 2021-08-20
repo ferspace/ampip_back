06095952ef0b3caf59697604ee31f0fc2e8be74e
edyMartin2 <evalerio.sdc@hotmail.com>
2021-08-20 13:13:04 -0500

    okey

--- app/controllers/api/v1/rescue_user_information_controller.rb
+++ app/controllers/api/v1/rescue_user_information_controller.rb
@@ -2,8 +2,17 @@ class Api::V1::RescueUserInformationController < ApplicationController
 
     def show
         dataUser = UserInformation.joins(:user).where(users: {id: params[:id]})
-        n = dataUser.merge!(User.find(params[:id]))
-        render json: n
+        if dataUser[0] != nil
+            corporate = Corporate.where(id: dataUser[0].id)
+            users = User.where(id: dataUser[0].user_id)
+            if corporate[0] != nil
+                render json: {"user": dataUser[0],"informacion": users.first, "corporate": corporate[0]}
+            else
+                render json: dataUser, status: :ok
+            end
+        else
+            render json: {}, status: 404
+        end
     end
 
 end
