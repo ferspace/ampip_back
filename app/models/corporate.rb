4f768768d0a31bbddbd7f7729b691a0346141515
edyMartin2 <evalerio.sdc@hotmail.com>
2021-08-23 11:18:21 -0500

    listo

--- app/models/corporate.rb
+++ app/models/corporate.rb
@@ -2,4 +2,5 @@ class Corporate < ApplicationRecord
     has_many :corporate_informations, dependent: :destroy
     has_many :Properties , dependent: :destroy
     has_many :user_informations , dependent: :destroy 
+    enum corporate_type: [ :consultoría, :construcción, :energía, :financiero, :inmobiliario, :telecomunicaciones, :transporte, :otros ]
 end
