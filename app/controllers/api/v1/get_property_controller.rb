154ab4af34d8b0483f7ce1921be4614ce3c2e3be
edyMartin2 <evalerio.sdc@hotmail.com>
2021-09-20 10:36:23 -0500

    publicados

--- app/controllers/api/v1/get_property_controller.rb
+++ app/controllers/api/v1/get_property_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GetPropertyController < ApplicationController
     
 
     def index
-        property = PropertyInformations.joins(:property).where( property_informations:{status:2})
+        property = PropertyInformations.joins(:property).where( property_informations:{status:1})
         render json: property
     end
 
