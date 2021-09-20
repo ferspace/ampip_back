d08a59b2f95954d0d735f5d89c50aab48a083d8a
edyMartin2 <evalerio.sdc@hotmail.com>
2021-09-20 12:28:17 -0500

    ok

--- app/controllers/api/v1/search_image_controller.rb
+++ app/controllers/api/v1/search_image_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SearchImageController < ApplicationController
 
     def show
-        property = PropertyInformations.where(property_id:params[:id])
+        property = PropertyInformations.where(id:params[:id])
         render json: property
     end 
 
