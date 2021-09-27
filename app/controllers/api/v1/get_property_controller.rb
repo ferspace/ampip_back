bf9d3d2c267225f3d3c465a3ea5cb9aec3f6e88e
edyMartin2 <evalerio.sdc@hotmail.com>
2021-09-27 12:06:39 -0500

    propiedas a bilda

--- app/controllers/api/v1/get_property_controller.rb
+++ app/controllers/api/v1/get_property_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GetPropertyController < ApplicationController
     
 
     def index
-        property = PropertyInformations.joins(:property).where( property_informations:{status:1})
+        property = PropertyInformations.joins(:property).where( property_informations:{status:2})
         render json: property
     end
 
